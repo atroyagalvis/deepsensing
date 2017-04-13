0815c751fcadcc023b0600b1129a8f652c2b12d9
atroyagalvis <ac.troya@gmail.com>
2017-04-13 10:25:17 +0200

    added validation set
    Added validation data set, now two different generators are used for training and testing during training
    and a third dataset is used for the final validation of the model

--- scripts/deepsensing_categorical.py
+++ scripts/deepsensing_categorical.py
@@ -7,9 +7,10 @@ from keras import optimizers
 
 train_label_img = "./labelszh12.tif"
 test_label_img = "./labelszh16.tif"
+validation_label_img = './labelszh11c.tif'
 train_img = "./zh12.tif"
 test_img = "./zh16.tif"
-
+validation_img = './zh11c.tif'
 img_bands, img_rows, img_cols, batch_size, nb_classes = 4, 8, 8, 32, 7
 
 #load the VGG16 model fully trainable
@@ -17,11 +18,12 @@ model = models.basic_cnn_categorical(img_bands, img_rows, img_cols, nb_classes=n
 model.compile(optimizer='adam', loss='categorical_crossentropy')
 #generate training data (X,y)
 generatordata = utils.batchGenerator(train_img, train_label_img, tilesize=img_rows, batch_size=batch_size, random=True, categorical=True, nb_classes=nb_classes)
+generatordata_test = utils.batchGenerator(train_img, train_label_img, tilesize=img_rows, batch_size=batch_size, random=True, categorical=True, nb_classes=nb_classes)
 #fit the model
-utils.fit_model(model,generatordata,epocs = 1000, samples_per_epoch=200, nb_val_samples=10,early_stopping=True, patience=5)
+utils.fit_model(model,generatordata,generatordata_test,epocs = 1000, samples_per_epoch=30000, nb_val_samples=10,early_stopping=True, patience=5)
 weights_path = 'weights.h5'
 model.load_weights(weights_path, by_name = True)
 #make a prediction on the test image
-utils.predict_image_categorical_tile(model,test_img,tilesize=img_rows, outputimg = "prediction.tif")
-print("accuracy : "+str(utils.evaluate("./prediction.tif",test_label_img)))
+utils.predict_image_categorical_tile(model,validation_img,tilesize=img_rows, outputimg = "prediction_categorical_center.tif")
+print("accuracy : "+str(utils.evaluate("./prediction_categorical_center.tif",validation_label_img)))
 
